76863f6d2a063bd5d961d93a85076e4d1b74e463
Renison Souza <souza.renison@gmail.com>
2023-12-02 22:57:22 -0300

    Ajustes

--- control/InstallAPI.js
+++ control/InstallAPI.js
@@ -6,6 +6,7 @@ const VinhoModel = require('../models/vinho');
 const UsuarioModel = require('../models/usuario');
 
 router.get('/', async (req, res) => {
+
     await sequelize.sync({ force: true });
 
     let produtores = [
@@ -17,16 +18,20 @@ router.get('/', async (req, res) => {
     }
 
     let vinho1 = await VinhoModel.save("Vinho 1", 2020, vetProdutores[0].codigo);
-    let vinho2 = await VinhoModel.save("Vinho 2", 2019, vetProdutores[1].codigo);
-    let vinho3 = await VinhoModel.save("Vinho 3", 2018, vetProdutores[2].codigo);
+    let vinho2 = await VinhoModel.save("Vinho 2", 1997, vetProdutores[1].codigo);
+    let vinho3 = await VinhoModel.save("Vinho 3", 1984, vetProdutores[2].codigo);
+    let vinho4 = await VinhoModel.save("Vinho 4", 2012, vetProdutores[3].codigo);
+    let vinho5 = await VinhoModel.save("Vinho 5", 2014, vetProdutores[4].codigo);
 
-    let vetVinhos = [vinho1, vinho2, vinho3];
+    let vetVinhos = [vinho1, vinho2, vinho3, vinho4, vinho5];
 
     let usuario1 = await UsuarioModel.save("Nome1", "Usuario1", "senha1");
     let usuario2 = await UsuarioModel.save("Nome2", "Usuario2", "senha2");
     let usuario3 = await UsuarioModel.save("Nome3", "Usuario3", "senha3");
+    let usuario4 = await UsuarioModel.save("Nome4", "Usuario4", "senha4");
+    let usuario5 = await UsuarioModel.save("Nome5", "Usuario5", "senha5");
 
-    let vetUsuarios = [usuario1, usuario2, usuario3];
+    let vetUsuarios = [usuario1, usuario2, usuario3, usuario4, usuario5];
 
     res.json({ status: true, produtores: vetProdutores, vinhos: vetVinhos, usuarios: vetUsuarios });
 });
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-require('dotenv').config();
 
+require('dotenv').config();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello, World!');
 });
 
 app.use("/install", require('./control/InstallAPI'))
+app.use("/login",  require('./control/usuarioAPI'));
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Rodando na porta ${PORT}`);
